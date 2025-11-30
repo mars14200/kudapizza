3e66b858ece690da7bb58125527839ca559f7f0a
Anzhelika <a.fidishchenko.dodo@gmail.com>
2025-11-30 11:54:23 +0500

    popup

--- script.js
+++ script.js
@@ -24,5 +24,30 @@ document.querySelector('.info__link').addEventListener('click', function () {
     document.querySelector('.info__text').classList.toggle('info__text--active');
     buttonText = this.textContent;
     this.textContent = buttonText2;
-    buttonText2 =  buttonText;
+    buttonText2 = buttonText;
 })
+
+// ждем полной загрузки страницы 
+document.addEventListener('DOMContentLoaded', function () {
+    const buttonPopup = document.querySelector('.catalog__filter');
+    if (buttonPopup) {
+        buttonPopup.addEventListener('click', function () {
+            const popupFilter = document.querySelector('.popup-filter');
+            if (popupFilter) {
+                document.body.classList.add('no-scroll');
+                popupFilter.classList.add('popup-filter--show')
+                setTimeout(function () {
+                    popupFilter.classList.add('popup-filter--active')
+                }, 10);
+            }
+        })
+    }
+    const buttonPopupClose = document.querySelector('.popup-filter__close');
+    if (buttonPopupClose) {
+        buttonPopupClose.addEventListener('click', function () {
+            popupFilter.classList.remove('popup-filter--active')}
+        )
+        }}
+
+)
+   
