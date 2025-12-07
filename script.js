86f102a98d9576897691685294093322d4121eeb
Anzhelika <a.fidishchenko.dodo@gmail.com>
2025-12-07 08:58:38 +0500

    knopki v javascript

--- script.js
+++ script.js
@@ -27,27 +27,111 @@ document.querySelector('.info__link').addEventListener('click', function () {
     buttonText2 = buttonText;
 })
 
-// ждем полной загрузки страницы 
+// ждем полной загрузки страницы
 document.addEventListener('DOMContentLoaded', function () {
-    const buttonPopup = document.querySelector('.catalog__filter');
-    if (buttonPopup) {
-        buttonPopup.addEventListener('click', function () {
-            const popupFilter = document.querySelector('.popup-filter');
-            if (popupFilter) {
+    const popupFilter = document.querySelector('.popup-filter');
+
+    if (popupFilter) {
+        const buttonPopup = document.querySelector('.catalog__filter');
+        if (buttonPopup) {
+            buttonPopup.addEventListener('click', function () {
                 document.body.classList.add('no-scroll');
                 popupFilter.classList.add('popup-filter--show')
                 setTimeout(function () {
                     popupFilter.classList.add('popup-filter--active')
                 }, 10);
-            }
-        })
-    }
-    const buttonPopupClose = document.querySelector('.popup-filter__close');
-    if (buttonPopupClose) {
-        buttonPopupClose.addEventListener('click', function () {
-            popupFilter.classList.remove('popup-filter--active')}
-        )
-        }}
+            })
+        }
 
-)
-   
+        const buttonPopupClose = document.querySelector('.popup-filter__close');
+        if (buttonPopupClose) {
+            buttonPopupClose.addEventListener('click', function () {
+                popupFilter.classList.remove('popup-filter--active')
+                setTimeout(function () {
+                    popupFilter.classList.remove('popup-filter--show')
+                    document.body.classList.remove('no-scroll');
+                }, 300);
+            });
+        }
+    }
+
+    let titles = [
+        "Общее",
+        "Сыр",
+        "Мясо",
+        "Компонент"
+    ];
+
+    let buttons = [
+        [
+            "Хит",
+            "Новинка",
+            "С мясом",
+            "Вегетарианская",
+            "С Курицей",
+            "Без лука",
+            "С грибами",
+            "С морепродуктами",
+            "Барбекью"
+        ],
+        [
+        "Реджанито",
+        "Моцарелла",
+        "Чеддер",
+        "С голубой плесенью",
+        "Смесь итальянских сыров",
+        "Мягкий молодой сыр"
+        ],
+        [
+        "Пепперони",
+        "Свинина",
+        "Ветчина",
+        "Бекон"
+        ],
+        [
+        "Креветка",
+        "Ананасы",
+        "Шампиньоны",
+        "Лук",
+        "Перец халапеньо",
+        "Орегано",
+        "Зеленый перец",
+        "Томаты",
+        "Чеснок",
+        "Красный перец",
+        "Оливки",
+        "Маслины",
+        "Клубника",
+        "Смесь итальянских трав"
+        ]
+    ];
+
+    const groups = document.querySelector('.popup-filter__groups');
+    if (groups) {
+        let groupHtml = '';
+        for (let i = 0; i < titles.length; i++) {
+            let buttonsHtml = '';
+            for (let j = 0; j < buttons[i].length; j++) {
+                buttonsHtml = buttonsHtml + `<button>${buttons[i][j]}</button>`
+            }
+            groupHtml = groupHtml + `<div class="popup-filter__group"> 
+                        <h4>${titles[i]}</h4>
+                        <div class="popup-filter__checkbox"> 
+                        ${buttonsHtml}
+                        </div>
+                    </div>`;
+        }
+        groups.innerHTML = groupHtml;
+    }
+});
+
+
+//let m = [5, 4, 5, 2];
+//console.log(m[0])
+//m[1] = 7
+//console.log(m);
+//m[10] = 9
+//console.log(m[10])
+//m.push(588)
+//m.length
+
